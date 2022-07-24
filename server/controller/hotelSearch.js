f8d730382e109d0ec6ad2d305bb13b078765d6cd
abdelaziz <abdelazizt320@gmail.com>
2022-07-24 11:41:31 +0200

    hotel data json file added, get hotels route added

--- server/controller/hotelSearch.js
+++ server/controller/hotelSearch.js
@@ -1,33 +1,37 @@
 const Amadeus = require("amadeus"),
-AppError = require('../utils/appError');
+    AppError = require('../utils/appError');
 require('dotenv').config();
 const clientId = process.env.AMADEUS_API_KEY;
 const clientSecret = process.env.AMADEUS_SECRET_KEY;
 // create amadeus API client
 const amadeusClient = new Amadeus({ clientId, clientSecret });
 //suggest city on city search
-const suggestCity = async (req, res,next) => {
-  
+const path = require('path');
+var fs = require("fs").promises
+const suggestCity = async (req, res, next) => {
     try {
         const { keyword } = req.query;
         const response = await amadeusClient.referenceData.locations.get({
             keyword,
             subType: Amadeus.location.city,
         });
-        const {data}=JSON.parse(response.body)
-        const addressList =data.map(e=>e.address)
+        const { data } = JSON.parse(response.body)
+        const addressList = data.map(e => e.address)
         res.status(200).json(addressList);
     } catch (error) {
-        const {description:{error_description}}=error
+        const { description: { error_description } } = error
         next(new AppError(error_description, 406));
     }
 }
-
-
 // get hotels in selected city
-const getCityHotels=async()=>{
-
+const getCityHotels = async (req, res, next) => {
+    try {
+        const usersFilePath = path.join(__dirname, '../data/hotelsData.json');
+        const data = await fs.readFile(usersFilePath, 'utf-8')
+        res.header("Content-Type", 'application/json');
+        res.status(200).send(data);
+    } catch (error) {
+        next(new AppError(error, 406));
+    }
 }
-
-
-module.exports = { suggestCity }
+module.exports = { suggestCity, getCityHotels }
--- server/routes/hotel.js
+++ server/routes/hotel.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const  router =express.Router()
 
-const {suggestCity }=require('../controller/hotelSearch.js');
+const {suggestCity,getCityHotels}=require('../controller/hotelSearch.js');
 
 router.get('/search',suggestCity)
-
+router.get('/hotels',getCityHotels)
 module.exports = router;
